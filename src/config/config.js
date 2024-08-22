a000456b0dfe215720cc56fca33b904c60c49dbe
Gabriel Henrique <henriquedeveloper24@gmail.com>
2024-08-22 10:42:40 -0300

    fix: rotas funcionando

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,4 @@
-const { PORTA = 6543,
-    HOST = 'aws-0-us-west-1.pooler.supabase.com',
-    USUARIO = 'postgres.pyfqdcxapmkceocjyeou',
-    SENHA = 'projeto-backend',
-    BANCO = 'postgres', } = process.env
+const { PORTA, HOST, USUARIO, SENHA, BANCO } = process.env
 console.log(`postgres://${USUARIO}:${SENHA}@${HOST}:${PORTA}/${BANCO}`);
 
 module.exports = {
